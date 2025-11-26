8137fae5b0a21a09e49050c6ab995f71a8074323
seung0910 <seung0910@cafe24.com>
2025-11-27 00:05:03 +0900

    운영배보

--- web.js
+++ web.js
@@ -1,7 +1,10 @@
-const http = require('http');
+import http from 'http';
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
   res.end('<h1>안녕하세요! seung0910 사이트입니다!</h1>');
 });
+
 const port = process.env.PORT || 3000;
-server.listen(port);
+server.listen(port, () => {
+  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
+});
