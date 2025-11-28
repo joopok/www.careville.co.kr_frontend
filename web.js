8706bd7ad94755694faee7c75974a47321f3e337
seung0910 <seung0910@cafe24.com>
2025-11-28 22:57:35 +0900

    package 수정

--- web.js
+++ web.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
-
 // Cafe24에서 지정한 포트 사용 (환경변수에서 가져옴)
 const PORT = process.env.PORT || 8001;
-
 app.get('/', (req, res) => {
     res.send('안녕하세요! Node.js 서버가 실행 중이에요!');
 });
