eabfb27a81c97c8e962c423fdb23edd29027297e
seung0910 <seung0910@cafe24.com>
2025-12-03 00:27:41 +0900

    web.js 파일수정

--- web.js
+++ web.js
@@ -8,7 +8,6 @@ import { promisify } from 'util';
 const execAsync = promisify(exec);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const app = express();
 const PORT = process.env.PORT || 8001;
 
