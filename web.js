5f9533466e04779948d11db2045e90591ab96a37
seung0910 <seung0910@cafe24.com>
2025-12-03 00:38:48 +0900

    추가개발

--- web.js
+++ web.js
@@ -8,6 +8,7 @@ import { promisify } from 'util';
 const execAsync = promisify(exec);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 8001;
 
